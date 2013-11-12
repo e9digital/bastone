d603b33642707f5b253e0a016412d54fd2e9f6ed
Travis <numbers1311407@gmail.com>
2013-11-11 23:25:06 -0500

    change stylesheet url lookups to roots theme dir

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9a23664efda415e4bfbc6816af12ba13');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9108984f4fd21a9555d9895849cdb4cf');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
