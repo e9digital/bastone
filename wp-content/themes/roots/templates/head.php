328cd5a9f17d6b55adc458297eb31707cc6f439a
Travis <numbers1311407@gmail.com>
2013-11-19 14:04:24 -0500

    set content-width in viewport metatag

--- wp-content/themes/roots/templates/head.php
+++ wp-content/themes/roots/templates/head.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="utf-8">
   <title><?php wp_title('|', true, 'right'); ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=1240">
   <?php wp_head(); ?>
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
