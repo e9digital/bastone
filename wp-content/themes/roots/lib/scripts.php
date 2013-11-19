e730ac31e609bd03bf40c3585d2e3508dc7c4426
Travis <numbers1311407@gmail.com>
2013-11-19 14:26:50 -0500

    change interior hover overlay and increase font size

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'da10b8445204764aed0804eeac070fb8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0edb8a08b0d2e4f8ef5c97ea5a3b6bbd');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
